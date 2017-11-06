049e8f7257f021efb68e15a5779a7de9af92a274
CODE-MAC1 <developer.codeid@gmail.com>
2017-11-06 13:43:44 +0700

    Initial Commit

---
+++ Newscover/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Newscover
+//
+//  Created by CODE-MAC1 on 11/6/17.
+//  Copyright © 2017 PT. Code Development Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
