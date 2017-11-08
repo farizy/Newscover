3c42875e8621bc17d012d9beda7b11d8c7749083
Ahmad Farizy <ahmad@code.id>
2017-11-08 17:40:35 +0700

    Add tableView to show article list using RxCocoa

--- Newscover/ViewController.swift
+++ Newscover/ViewController.swift
@@ -8,32 +8,41 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
     let viewModel = ViewModel()
     let disposeBag = DisposeBag()
     
     @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.getArticle()
         bindArticles()
+        setupCellTapHandling()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     func bindArticles() {
+        
         viewModel.articles
             .asObservable()
             .observeOn(MainScheduler.instance)
-            .subscribe(onNext: { [weak self](articles) in
-                self?.textLabel.text = articles.first?.title
-                })
+            .bind(to: tableView.rx.items(cellIdentifier: "cell", cellType: UITableViewCell.self)){
+                row, article, cell in
+                cell.textLabel?.text = article.title
+            }
+//            .subscribe(onNext: { [weak self](articles) in
+//                self?.textLabel.text = articles.first?.title
+//                })
             .addDisposableTo(disposeBag)
-        
+
         viewModel.errorObserver
             .asObservable()
             .observeOn(MainScheduler.instance)
@@ -42,8 +51,27 @@ class ViewController: UIViewController {
                 let noAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
                 alert.addAction(noAction)
                 self?.present(alert, animated: true, completion: nil)
+
+                self?.textLabel.isHidden = false
             })
             .addDisposableTo(disposeBag)
     }
+    
+    func setupCellTapHandling() {
+        tableView
+            .rx
+            .modelSelected(Article.self)
+            .subscribe({ [weak self] (article) in
+                guard let article = article.element else { return }
+                let alert = UIAlertController(title: article.title, message: article.description, preferredStyle: UIAlertControllerStyle.alert)
+                let noAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil)
+                alert.addAction(noAction)
+                self?.present(alert, animated: true, completion: nil)
+                
+                if let selectedRowIndexPath = self?.tableView.indexPathForSelectedRow{
+                    self?.tableView.deselectRow(at: selectedRowIndexPath, animated: true)
+                }
+            })
+        .addDisposableTo(disposeBag)
+    }
 }
-
