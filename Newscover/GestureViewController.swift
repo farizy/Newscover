93c1b39b82889426317fb172bf5262d122094961
Ahmad Farizy <ahmad@code.id>
2017-11-20 14:46:40 +0700

    Add view at Gesture VC to show webView

--- Newscover/GestureViewController.swift
+++ Newscover/GestureViewController.swift
@@ -9,13 +9,17 @@
 import UIKit
 import RxSwift
 import NVActivityIndicatorView
+import WebKit
 
 class GestureViewController: UIViewController, NVActivityIndicatorViewable {
     @IBOutlet weak var gestureImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    
-    
+    @IBOutlet weak var topViewConstraint: NSLayoutConstraint!
+    @IBOutlet weak var dissmissViewButton: UIButton!
+    @IBOutlet weak var webViewContainer: UIView!
+    var contentWebView: WKWebView = WKWebView()
+
     let progressView = CGSize(width: 50, height: 50)
     var viewModel: GestureViewModel?
     let disposeBag = DisposeBag()
@@ -35,7 +39,7 @@ class GestureViewController: UIViewController, NVActivityIndicatorViewable {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureView()
         addGesture()
         guard let viewModel = viewModel else{
             return
@@ -50,6 +54,32 @@ class GestureViewController: UIViewController, NVActivityIndicatorViewable {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func configureView() {
+        let height = self.view.frame.height
+        topViewConstraint.constant = height
+        self.view.layoutIfNeeded()
+        
+        dissmissViewButton.addTarget(self, action: #selector(dissmissView(_:)), for: .touchUpInside)
+        
+        webViewContainer.clipsToBounds = true
+        
+        contentWebView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+        contentWebView.translatesAutoresizingMaskIntoConstraints = false
+        contentWebView.scrollView.layer.masksToBounds = false
+        
+        webViewContainer.addSubview(contentWebView)
+        
+        let constraints = [
+            contentWebView.topAnchor.constraint(equalTo: webViewContainer.topAnchor),
+            contentWebView.bottomAnchor.constraint(equalTo: webViewContainer.bottomAnchor),
+            contentWebView.leadingAnchor.constraint(equalTo: webViewContainer.leadingAnchor),
+            contentWebView.trailingAnchor.constraint(equalTo: webViewContainer.trailingAnchor)
+        ]
+        constraints.forEach({ anchor in
+            anchor.isActive = true
+        })
     }
     
     func addGesture() {
@@ -108,8 +138,9 @@ class GestureViewController: UIViewController, NVActivityIndicatorViewable {
         descriptionLabel.text = article.description
         gestureImageView.af_setImage(withURL: article.urlToImage)
         shadowedFont()
-
-    
+        
+        let request = URLRequest(url: article.url)
+        contentWebView.load(request)
     }
     
     func shadowedFont(){
@@ -152,16 +183,29 @@ class GestureViewController: UIViewController, NVActivityIndicatorViewable {
         guard let gesture = gesture as? UISwipeGestureRecognizer else { return }
         
         if gesture.direction == UISwipeGestureRecognizerDirection.up {
-            guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetailViewControllerID") as? DetailViewController else { return }
-            let article = viewModel?.articles.value[index]
+//            guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "DetailViewControllerID") as? DetailViewController else { return }
+//            let article = viewModel?.articles.value[index]
+//
+//            vc.url = article?.url
+//            self.present(vc, animated: true, completion: nil)
 
-            vc.url = article?.url
-            self.present(vc, animated: true, completion: nil)
+            UIView.animate(withDuration: 1.0, animations: {
+                self.topViewConstraint.constant = 0.0
+                self.view.layoutIfNeeded()
+            })
         }
         else if gesture.direction == UISwipeGestureRecognizerDirection.down {
             print("Swipe Down")
             self.dismiss(animated: true, completion: nil)
         }
+    }
+    
+    func dissmissView(_ sender: UIButton) {
+        let height = self.view.frame.height
+        UIView.animate(withDuration: 1.0, animations: {
+            self.topViewConstraint.constant = height
+            self.view.layoutIfNeeded()
+        })
     }
     
 }
